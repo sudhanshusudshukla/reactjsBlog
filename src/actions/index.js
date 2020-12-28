1d2f237e8cbd1e85a8524d7d83178ae0d2ca758e
sudhanshusamshukla <sudhanshu.shukla@outlook.com>
2020-12-28 12:26:07 +0530

    react

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,11 +1,11 @@
 import jsonPlaceholder from  '../api/JSONPlaceholder';
 
-export const fetchPosts = async () => {
-
- const response = await jsonPlaceholder.get('/posts')
+export const fetchPosts =  () => {
+// Bad approach!!!
+ const promise =  jsonPlaceholder.get('/posts')
 
     return {
         type: 'FETCH_POSTS',
-        payload: response
+        payload: promise
     };
 };
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux'
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 
 import App from './components/App';
 import reducers from './reducers/index';
